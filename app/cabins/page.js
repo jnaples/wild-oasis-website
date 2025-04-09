dca1050dbf16648665a14b7be425c6f718c55d73
dojo <jnaples90@gmail.com>
2025-04-09 05:45:49 -0400

    first commit

---
+++ app/cabins/page.js
@@ -0,0 +1,13 @@
+import Counter from "@/app/_components/Counter";
+
+export const metadata = {
+  title: "Cabins",
+};
+
+export default function Page() {
+  return (
+    <div>
+      <h1>Cabin page</h1>
+    </div>
+  );
+}
